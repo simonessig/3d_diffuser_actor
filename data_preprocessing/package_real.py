updated viz; rotation fixes; azure real env

data_preprocessing/package_real.py
@@ -3,23 +3,21 @@ import pickle
 from pathlib import Path
 
 import blosc
-import cv2
 import numpy as np
 import tap
 import torch
 from PIL import Image
-from scipy.spatial.transform import Rotation
 from tqdm import tqdm
 
-from utils.utils_with_real import deproject, get_cam_info, keypoint_discovery, viz_pcd
+from utils.utils_with_real import get_cam_info, process_kinect, viz_pcd
 
 
 class Arguments(tap.Tap):
     data_dir: Path = Path(__file__).parent.parent / "data/real/raw"
     seed: int = 15
     task: str = "pick_box"
-    split_train: float = 7
-    split_test: float = 3
+    split_train: float = 8
+    split_test: float = 2
     split_val: float = 2
     image_size: str = "256,256"
     output: Path = Path(__file__).parent.parent / "data/real/packaged"
@@ -42,8 +40,8 @@ def load_episode(data_dir, datas, args, cam_info, ann_id):
     ee_pos = torch.load(f"{data_dir}/ee_pos.pt").numpy()
     gripper_command = torch.load(f"{data_dir}/gripper_command.pt").numpy()
 
-    # From quaternion to euler angles
-    ee_euler = Rotation.from_quat(ee_pos[:, 3:7]).as_euler("xyz")
+    ee_pos[:, 3:7] += np.array([0, 0.38, 0, 0])
+    ee_pos[:, 3:7] = ee_pos[:, [6, 3, 4, 5]]
 
     # Map gripper openess to [0, 1]
     gripper_command = (gripper_command > 0).astype(np.float32)[:, None]
@@ -51,8 +49,7 @@ def load_episode(data_dir, datas, args, cam_info, ann_id):
     proprio = list(
         np.concatenate(
             [
-                ee_pos[:, :3],
-                ee_euler,
+                ee_pos[:, :7],
                 gripper_command,
             ],
             axis=-1,
@@ -63,39 +60,22 @@ def load_episode(data_dir, datas, args, cam_info, ann_id):
     rgb_dir = data_dir / "img" / "cam0_rgb"
     rgb_path_gen = sorted(rgb_dir.glob("*.png"), key=lambda x: int(x.name[:-4]))
 
+    # sorted depth images
+    depth_dir = data_dir / "img" / "cam0_d"
+    depth_path_gen = sorted(depth_dir.glob("*.png"), key=lambda x: int(x.name[:-4]))
+
     # sorted pointclouds
     pcd_dir = data_dir / "img" / "cam0_pc"
     pcd_path_gen = sorted(pcd_dir.glob("*.pt"), key=lambda x: int(x.name[:-3]))
 
     rgbs = []
     pcds = []
-    for rgb_path, pcd_path in zip(rgb_path_gen, pcd_path_gen):
+    for rgb_path, depth_path, pcd_path in zip(rgb_path_gen, depth_path_gen, pcd_path_gen):
         rgb = np.array(Image.open(rgb_path))
+        depth = np.array(Image.open(depth_path))
         pcd = torch.load(pcd_path).numpy()
 
-        h, w = rgb.shape[0], rgb.shape[1]
-
-        xa, xb, ya, yb = 0, 150, 280, 280
-
-        # crop to square
-        rgb = rgb[xa : h - xb, int((w - h) / 2) + ya : int((w + h) / 2) - yb]
-        pcd = pcd[xa : h - xb, int((w - h) / 2) + ya : int((w + h) / 2) - yb]
-
-        rgb = cv2.resize(rgb, img_dim)
-        pcd = cv2.resize(pcd, img_dim)
-
-        rgb = rgb / 255.0 * 2 - 1  # map RGB to [-1, 1]
-        pcd /= 1000  # mm to m
-
-        # camera extrinsics
-        pcd = np.reshape(pcd, (img_dim[0] * img_dim[1], 3))
-        y, z, x = pcd.T
-        cam_pos = np.stack([x, y, -z, np.ones_like(z)], axis=0)
-        pcd = cam_info[1] @ cam_pos
-        pcd = np.reshape(pcd[:3].T, (img_dim[0], img_dim[1], 3))
-
-        rgb[pcd[:, :, 0] < 0] = 0
-        pcd[pcd[:, :, 0] < 0] = 0
+        rgb, pcd = process_kinect(rgb, pcd, img_dim, cam_info, depth=depth)
 
         rgbs.append(rgb)
         pcds.append(pcd)
@@ -106,7 +86,7 @@ def load_episode(data_dir, datas, args, cam_info, ann_id):
     # Put them into a dict
     datas["pcd"] += pcds  # (*img_dim, 3)
     datas["rgb"] += rgbs  # (*img_dim, 3)
-    datas["proprios"] += proprio  # (7,)
+    datas["proprios"] += proprio  # (8,)
     datas["annotation_id"].append(ann_id)  # int
 
     return datas

datasets/dataset_real.py
@@ -9,11 +9,10 @@ import torch
 
 from utils.utils_with_real import convert_rotation, to_relative_action
 
-from .dataset_engine import RLBenchDataset
 from .utils import Resize, TrajectoryInterpolator
 
 
-class RealDataset(RLBenchDataset):
+class RealDataset:
 
     def __init__(
         self,
@@ -34,7 +33,6 @@ class RealDataset(RLBenchDataset):
         return_low_lvl_trajectory=False,
         dense_interpolation=False,
         interpolation_length=100,
-        relative_action=False,
     ):
         self._cache = {}
         self._cache_size = cache_size
@@ -47,7 +45,6 @@ class RealDataset(RLBenchDataset):
         if isinstance(root, (Path, str)):
             root = [Path(root)]
         self._root = [Path(r).expanduser() for r in root]
-        self._relative_action = relative_action
 
         # For trajectory optimization, initialize interpolation tools
         if return_low_lvl_trajectory:
@@ -212,43 +209,6 @@ class RealDataset(RLBenchDataset):
             modals = self._resize(rgbs=rgbs, pcds=pcds)
             rgbs = modals["rgbs"]
             pcds = modals["pcds"]
-
-        # Compute relative action
-        if self._relative_action and traj is not None:
-            rel_traj = torch.zeros_like(traj)
-            for i in range(traj.shape[0]):
-                for j in range(traj.shape[1]):
-                    rel_traj[i, j] = torch.as_tensor(
-                        to_relative_action(traj[i, j].numpy(), traj[i, 0].numpy(), clip=False)
-                    )
-            traj = rel_traj
-
-        # Convert Euler angles to Quarternion
-        action = torch.cat(
-            [action[..., :3], torch.as_tensor(convert_rotation(action[..., 3:6])), action[..., 6:]],
-            dim=-1,
-        )
-        gripper = torch.cat(
-            [
-                gripper[..., :3],
-                torch.as_tensor(convert_rotation(gripper[..., 3:6])),
-                gripper[..., 6:],
-            ],
-            dim=-1,
-        )
-        gripper_history = torch.cat(
-            [
-                gripper_history[..., :3],
-                torch.as_tensor(convert_rotation(gripper_history[..., 3:6])),
-                gripper_history[..., 6:],
-            ],
-            dim=-1,
-        )
-        if traj is not None:
-            traj = torch.cat(
-                [traj[..., :3], torch.as_tensor(convert_rotation(traj[..., 3:6])), traj[..., 6:]],
-                dim=-1,
-            )
 
         ret_dict = {
             "task": [task for _ in frame_ids],

eval_real/utils/envs/env_interface.py
@@ -36,18 +36,26 @@ class EnvInterface(abc.ABC):
         self, rgb_img, depth_img, ee_pos, gripper_command
     ) -> Tuple[torch.tensor, torch.tensor, torch.tensor]:
         rgb = np.array(rgb_img)
+        h, w = rgb.shape[0], rgb.shape[1]
+        rgb = rgb[:, int((w - h) / 2) : int((w + h) / 2)]  # crop to square
         rgb = cv2.resize(rgb, self.image_size)
         rgb = rgb / 255.0 * 2 - 1  # map RGB to [-1, 1]
         rgb = einops.rearrange(rgb, "h w d -> d h w")[None, None, :, :, :]
+        # print(rgb)
 
         pcd = np.array(depth_img) / 1000
+        pcd = pcd[:, int((w - h) / 2) : int((w + h) / 2)]  # crop to square
         pcd = cv2.resize(pcd, self.image_size)
+        pcd[pcd > 2] = 2.0
+        pcd = cv2.medianBlur(pcd.astype(np.float32), 5)
+        pcd = cv2.medianBlur(pcd.astype(np.float32), 5)
+        pcd = cv2.medianBlur(pcd.astype(np.float32), 5)
         pcd = deproject(pcd, *self.cam_info).transpose(1, 0)
         pcd = np.reshape(pcd, (*self.image_size, 3))
         pcd = einops.rearrange(pcd, "h w d -> d h w")[None, None, :, :, :]
 
         # From quaternion to euler angles
-        ee_euler = Rotation.from_quat(ee_pos[3:7]).as_euler("xyz")
+        ee_euler = Rotation.from_quat([ee_pos[6], *ee_pos[3:6]]).as_euler("xyz")
 
         proprio = np.concatenate([ee_pos[:3], ee_euler, [gripper_command]])[None, :]
         return (

eval_real/utils/envs/real_robot.py
@@ -2,22 +2,23 @@ import concurrent
 import time
 from typing import Tuple
 
+import numpy as np
 import torch
 from polymetis import GripperInterface  # type: ignore
 from polymetis import RobotInterface  # type: ignore
 
 from eval_real.utils.envs.env_interface import EnvInterface
+from eval_real.utils.real_robot.azure import Azure
 from eval_real.utils.real_robot.realsense import Realsense
 
 
 class RealRobotInterface(EnvInterface):
     def __init__(self, image_size, cam_calib_file) -> None:
         super().__init__(image_size, cam_calib_file)
-        self.robot = RobotInterface(
-            ip_address="10.10.10.210", enforce_version=False, port=50051
-        )
+        self.robot = RobotInterface(ip_address="10.10.10.210", enforce_version=False, port=50051)
         self.gripper = GripperInterface(ip_address="10.10.10.210", port=50052)
-        self.cam = Realsense("cam0")
+        self.cam = Azure("cam0", self.image_size, self.cam_info)
+        # self.cam = Realsense("cam0", self.image_size, self.cam_info)
 
         self.max_width = 0.0
         self.min_width = 0.0
@@ -25,6 +26,16 @@ class RealRobotInterface(EnvInterface):
         self.within_grasp_action = False
 
         self.pool = concurrent.futures.ThreadPoolExecutor(1)
+
+    def prepare(self, _):
+        self._grasp()
+        self._open()
+        self.robot.move_to_joint_positions(
+            [-0.1271, -0.0062, 0.1186, -2.2072, 0.0027, 2.1948, 0.7662]
+        )
+        # self._grasp()
+        # time.sleep(5)
+        # self._open()
 
     def connect(self) -> None:
         if self.gripper.metadata:
@@ -36,25 +47,24 @@ class RealRobotInterface(EnvInterface):
 
         # self.robot.go_home()
 
-        self.robot.move_to_joint_positions(
-            [-0.1271, -0.0062, 0.1186, -2.2072, 0.0027, 2.1948, 0.7662]
-        )
-
-        self._grasp()
-        self._open()
-
         self.cam.connect()
 
     def get_obs(self) -> Tuple[torch.tensor, torch.tensor, torch.tensor]:
+        rgb, pcd = self.cam.get_obs()
+
         ee_pos = torch.cat(self.robot.get_ee_pose())
 
         gripper_state = self.gripper.get_state()
         gripper_command = 0.0 if gripper_state.is_grasped else 1.0
 
-        sensor_state = self.cam.get_sensors()
+        ee_pos[3:7] += torch.tensor([0, 0.38, 0, 0])
+        ee_pos[3:7] = ee_pos[[6, 3, 4, 5]]
 
-        return self._prepare_obs(
-            sensor_state["rgb"], sensor_state["depth"], ee_pos, gripper_command
+        proprio = np.concatenate([ee_pos[:7], [gripper_command]])[None, :]
+        return (
+            rgb,
+            pcd,
+            torch.tensor(proprio, dtype=torch.float32),
         )
 
     def move(self, action) -> None:
@@ -66,8 +76,6 @@ class RealRobotInterface(EnvInterface):
             self._open()
         else:
             self._grasp()
-
-        print(act_pos)
 
         self.robot.move_to_ee_pose(act_pos, None)
 

eval_real/utils/real_robot/realsense.py
@@ -1,18 +1,21 @@
-import logging
 import time
+from typing import Tuple
 
 import cv2
+import einops
 import numpy as np
 import pyrealsense2 as rs
+import torch
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+from utils.utils_with_real import deproject
 
 
 class Realsense:
-    def __init__(self, name: str):
+    def __init__(self, name: str, image_size, cam_info):
         self.name = name
         self.pipeline = None
+        self.image_size = image_size
+        self.cam_info = cam_info
 
     def connect(self) -> bool:
         print("Connecting to {}: ".format(self.name))
@@ -43,6 +46,32 @@ class Realsense:
         timestamp = time.time()
 
         return {"time": timestamp, "rgb": rgb_img, "depth": depth_img}
+
+    def get_obs(self) -> Tuple[torch.tensor, torch.tensor, torch.tensor]:
+        sensor_state = self.get_sensors()
+
+        rgb = np.array(sensor_state["rgb"])
+        h, w = rgb.shape[0], rgb.shape[1]
+        rgb = rgb[:, int((w - h) / 2) : int((w + h) / 2)]  # crop to square
+        rgb = cv2.resize(rgb, self.image_size)
+        rgb = rgb / 255.0 * 2 - 1  # map RGB to [-1, 1]
+        rgb = einops.rearrange(rgb, "h w d -> d h w")[None, None, :, :, :]
+
+        pcd = np.array(sensor_state["depth"]) / 1000
+        pcd = pcd[:, int((w - h) / 2) : int((w + h) / 2)]  # crop to square
+        pcd = cv2.resize(pcd, self.image_size)
+        pcd[pcd > 2] = 2.0
+        pcd = cv2.medianBlur(pcd.astype(np.float32), 5)
+        pcd = cv2.medianBlur(pcd.astype(np.float32), 5)
+        pcd = cv2.medianBlur(pcd.astype(np.float32), 5)
+        pcd = deproject(pcd, *self.cam_info).transpose(1, 0)
+        pcd = np.reshape(pcd, (*self.image_size, 3))
+        pcd = einops.rearrange(pcd, "h w d -> d h w")[None, None, :, :, :]
+
+        return (
+            torch.tensor(rgb, dtype=torch.float32),
+            torch.tensor(pcd, dtype=torch.float32),
+        )
 
     def close(self):
         self.pipeline.stop()

tools/visualize_keypose_frames.py
@@ -14,7 +14,7 @@ import torch.nn.functional as F
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-import tools.pytorch3d_transforms as pytorch3d_transforms
+import utils.pytorch3d_transforms as pytorch3d_transforms
 
 matplotlib.use("agg")
 matplotlib.rcParams["figure.dpi"] = 128
@@ -173,6 +173,7 @@ def get_gripper_matrix_from_action(action: torch.Tensor, rotation_param="quat_fr
 
     if "quat" in rotation_param:
         quaternion = action[..., 3:7]
+        # print(quaternion)
         rotation = pytorch3d_transforms.quaternion_to_matrix(quaternion)
     else:
         rotation = compute_rotation_matrix_from_ortho6d(action[..., 3:9])
@@ -295,12 +296,13 @@ def visualize_actions_and_point_clouds(
 
     fig.tight_layout()
     ax.legend(loc="lower center", ncol=len(gripper_pose_trajs))
-    ax.set_xlim(-1, 1)
-    ax.set_ylim(-1, 1)
-    ax.set_zlim(-1, 1)
+    ax.set_xlim(0, 1)
+    ax.set_ylim(-0.5, 0.5)
+    ax.set_zlim(0, 1)
     images = []
     for elev, azim in zip(
-        [10, 15, 20, 25, 30, 25, 20, 15, 45, 90], [0, 45, 90, 135, 180, 225, 270, 315, 360, 360]
+        [10, 15, 20, 25, 30, 25, 20, 15, 45, 90],
+        [0, 45, 90, 135, 180, 225, 270, 315, 360, 360],
     ):
         ax.view_init(elev=elev, azim=azim, roll=0)
         canvas.draw()
@@ -310,7 +312,8 @@ def visualize_actions_and_point_clouds(
         image = cv2.resize(image, dsize=None, fx=0.75, fy=0.75)
         images.append(image)
     images = np.concatenate(
-        [np.concatenate(images[:5], axis=1), np.concatenate(images[5:10], axis=1)], axis=0
+        [np.concatenate(images[:5], axis=1), np.concatenate(images[5:10], axis=1)],
+        axis=0,
     )
     if save:
         Image.fromarray(images, mode="RGB").save("diff_traj.png")
@@ -323,7 +326,12 @@ def visualize_actions_and_point_clouds(
 
 
 def visualize_actions_and_point_clouds_video(
-    visible_pcd, visible_rgb, gt_pose, curr_pose, video=True, rotation_param="quat_from_query"
+    visible_pcd,
+    visible_rgb,
+    gt_pose,
+    curr_pose,
+    video=True,
+    rotation_param="quat_from_query",
 ):
     """Visualize by plotting the point clouds and gripper pose as video.
 
@@ -360,7 +368,11 @@ def visualize_actions_and_point_clouds_video(
         for img in images:
             pil_images.extend([Image.fromarray(img)] * 2)
         pil_images[0].save(
-            "keypose_frames.gif", save_all=True, append_images=pil_images[1:], duration=1, loop=0
+            "keypose_frames.gif",
+            save_all=True,
+            append_images=pil_images[1:],
+            duration=1,
+            loop=0,
         )
     else:
         for i, img in enumerate(images):

utils/utils_with_real.py
@@ -1,5 +1,6 @@
 import random
 
+import cv2
 import numpy as np
 import pyrealsense2
 import torch
@@ -8,6 +9,42 @@ from matplotlib import pyplot as plt
 from scipy.signal import argrelextrema
 
 import utils.pytorch3d_transforms as pytorch3d_transforms
+
+
+def process_kinect(rgb, pcd, img_dim, cam_info, depth=None):
+    h, w = rgb.shape[0], rgb.shape[1]
+
+    xa, xb, ya, yb = 0, 150, 230, 280
+
+    # crop to square
+    rgb = rgb[xa : h - xb, int((w - h) / 2) + ya : int((w + h) / 2) - yb]
+    pcd = pcd[xa : h - xb, int((w - h) / 2) + ya : int((w + h) / 2) - yb]
+
+    rgb = cv2.resize(rgb, img_dim)
+    pcd = cv2.resize(pcd, img_dim)
+
+    rgb = rgb / 255.0 * 2 - 1  # map RGB to [-1, 1]
+    pcd /= 1000  # mm to m
+
+    if depth is not None:
+        depth = depth[xa : h - xb, int((w - h) / 2) + ya : int((w + h) / 2) - yb]
+        depth = cv2.resize(depth, img_dim)
+        outliers = np.abs(depth - cv2.medianBlur(depth, 5)) > 10
+        pcd[outliers] = 0
+
+    # camera extrinsics
+    pcd = np.reshape(pcd, (img_dim[0] * img_dim[1], 3))
+    y, z, x = pcd.T
+    cam_pos = np.stack([x, y, -z, np.ones_like(z)], axis=0)
+    pcd = cam_info[1] @ cam_pos
+    pcd = np.reshape(pcd[:3].T, (img_dim[0], img_dim[1], 3))
+
+    rgb[pcd[:, :, 0] < 0] = 0
+    rgb[pcd[:, :, 0] > 0.8] = 0
+    pcd[pcd[:, :, 0] < 0] = 0
+    pcd[pcd[:, :, 0] > 0.8] = 0
+
+    return rgb, pcd
 
 
 def get_eef_velocity_from_trajectories(trajectories):
@@ -104,7 +141,7 @@ def get_cam_info(calib):
     offset = np.zeros((4, 4))
     mat = pytorch3d_transforms.euler_angles_to_matrix(torch.as_tensor([0, -0.035, 0.035]), "XYZ")
     offset[:3, :3] = mat.numpy()
-    offset[:3, 3] = np.array([0.01, 0.06, 0.08])
+    offset[:3, 3] = np.array([0.01, 0.05, 0.04])
     offset[3, 3] = 1.0
 
     return intrinsics, offset @ extrinsics
@@ -136,7 +173,7 @@ def deproject(depth_img, intrinsics, extrinsics):
     z, y, x = points.T
 
     cam_pos = np.stack([x, y, -z, np.ones_like(z)], axis=0)
-    print(cam_pos.shape)
+    # print(cam_pos.shape)
 
     world_pos = extrinsics @ cam_pos
     return world_pos[:3]